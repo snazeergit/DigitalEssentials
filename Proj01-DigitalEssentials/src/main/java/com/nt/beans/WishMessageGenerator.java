8888f1c71d88ece7543b87e603470a5614da50b3
Nazeer <nazeersd.java@gmail.com>
2022-06-10 00:31:39 +0530

    Coding done Dev

--- Proj01-DigitalEssentials/src/main/java/com/nt/beans/WishMessageGenerator.java
+++ Proj01-DigitalEssentials/src/main/java/com/nt/beans/WishMessageGenerator.java
@@ -1,5 +1,32 @@
 package com.nt.beans;
 
-public class WishMessageGenerator {
+import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("wmg")
+public class WishMessageGenerator {
+	@Autowired
+	private LocalDateTime ldt;
+	
+	
+	//Comment this and uncomment bean tag in cfg file
+	@Bean(name = "ldt")
+	public LocalDateTime getDateTime() {
+		return LocalDateTime.now();
+	}
+
+	public String getWishMessage() {
+		int hour = ldt.getHour();
+		if (hour < 12)
+			return "Good Morning";
+		else if (hour < 16)
+			return "Good Afternoon";
+		else if (hour < 20)
+			return "Good Eveing";
+		else
+			return "Good Night";
+	}
 }
